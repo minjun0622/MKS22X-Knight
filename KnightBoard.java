b3984e25d6c723faddfa07444e8d900543cf2256
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-02-14 14:20:52 -0500

    making revisions and adding useful comments.

--- KnightBoard.java
+++ KnightBoard.java
@@ -14,14 +14,17 @@ public KnightBoard(int r, int c){
 
   //Initialize the board to the correct size and make them all 0's
 public String toString(){
+  String result = "";
   for (int i = 0; i < board.length; i++){
     for (int x = 0; x < board.length; x++){
       board[i][x] = '_';
       if (Math.pow((board.length - 1) 2)){
         board[i][x] = //something with two digits.
+
       }
     }
   }
+}
 
 /*
 see format for toString below
@@ -63,6 +66,25 @@ Modifies the board by labeling the moves from 1 (at startingRow,startingCol) up 
 @returns true when the board is solvable from the specified starting position
 */
 
+
+
+/*
+moves for a knight in a chess piece.
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+moving up right - board[i - 2][x + 1]
+moving up left - board[i - 2][x - 1]
+moving down left - board[i + 2][i - 1]
+moving down right - board[i + 2][i + 1]
+moving right up - board[i + 1][x - 1]
+moving right down - board[i + 1][x + 1]
+moving left up - board[i - 1][x - 1]
+moving left down - board[i - 1][x + 1]
+*/
+
 public boolean solve(int r, int c){
   if (r <= 0 || r >= board.length || c <= 0 || c >= board.length) {
     throw IllegalArgumentException();
@@ -72,10 +94,22 @@ public boolean solve(int r, int c){
       if (board[i][x] != 0) {
         throw IllegalStateException();
       }
-
+      if (board[i + 1][x + 2] = 0){
+        board[i][x]++;
+      }
+      if (board[i + 2][x + 1] = 0){
+        board[i][x]++;
+      }
+      if (board[i - 1][x - 2] = 0){
+        board[i][x]++;
+      }
+      if (board[i - 2][x - 2] = 0){
+        board[i][x]++;
+      }
     }
+    return false;
   }
-
+  return true;
 }
 
 
