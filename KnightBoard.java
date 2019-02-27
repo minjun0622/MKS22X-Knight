e229e98f62154a2c8311335916afce7f99b1d632
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-02-27 02:13:25 -0500

    My head is going crazy.

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,7 +1,7 @@
 public class KnightBoard {
   private int[][] board;
   //array storing the moves of the board. My notes were converted basically.
-  private int[][] = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
+  private int[][] moves = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
   /*
   moving up right - board[i - 2][x + 1]
   moving up left - board[i - 2][x - 1]
@@ -57,7 +57,7 @@ Modifies the board by labeling the moves from 1 (at startingRow,startingCol) up 
 
 //similar fashion to queens. kinda easy.
 private boolean addKnight(int r, int c, int number){
-  if (r < 0 || r >= board.length || c < 0 || c >= board.length) {
+  if (r < 0 || r >= board.length || c < 0 || c >= board[0].length) {
     return false;
   }
   if (board[r][c] != 0){
@@ -69,7 +69,7 @@ private boolean addKnight(int r, int c, int number){
 
 //similar to addKnight except there's no number. If it can be removed, just remove.
 private boolean removeKnight(int r, int c){
-  if (r < 0 || r >= board.length || c < 0 || c >= board.length){
+  if (r < 0 || r > board.length || c < 0 || c > board[0].length){
     return false;
   }
   if (board[r][c] == 0) {
@@ -80,18 +80,18 @@ private boolean removeKnight(int r, int c){
 }
 
 public boolean solve(int r, int c){
-  if (r <= 0 || r >= board.length || c <= 0 || c >= board.length) {
-    throw new IllegalArgumentException();
-  }
   for (int i = 0; i < board.length; i++){
-    for(int x = 0; x < board[i].length; x++){
+    for(int x = 0; x < board[0].length; x++){
       if (board[i][x] != 0) {
         throw new IllegalStateException();
       }
     }
   }
-  board[r][c] = 1;
-  return solveH(r, c, 1);
+  if (r < 0 || r >= board.length || c < 0 || c >= board[0].length) {
+    throw new IllegalArgumentException();
+  }
+  addKnight(r,c,1);
+  return solveH(r, c, 2);
 }
 
 private boolean solveH(int r ,int c, int level){
@@ -121,31 +121,68 @@ m = 3 and n = 4, 6, or 8.
 */
 
 public int countSolutions(int r, int c){
-  if (r < 0 || c < 0 || r >= board.length || c >= board.length){
-    throw new IllegalArgumentException();
-  }
   for (int i = 0; i < board.length; i++){
-    for (int x = 0; x < board[i].length; x++){
+    for (int x = 0; x < board[0].length; x++){
       if (board[i][x] != 0){
         throw new IllegalStateException();
       }
     }
   }
-  return countHelper(r, c, 1);
+  if (r < 0 || c < 0 || r >= board.length || c >= board[0].length){
+    throw new IllegalArgumentException();
+  }
+  addKnight(r, c, 1);
+  return countHelper(r, c, 2);
 }
 
 private int countHelper(int r, int c, int number){
   int count = 0;
-  if (board[r][c] == 0) {
-    if (number == board.length * board[r].length){
-      return 1;
-    }
+  if (number == board.length * board[0].length + 1) {
+    return 1;
   }
-  for (int i = 0; i < board.length; i++){
+
+  for (int i = 0; i < moves.length; i++){
       if (addKnight(r + moves[i][0], c + moves[i][1], number)) {
         count += countHelper(r + moves[i][0], c + moves[i][1], number + 1);
         removeKnight(r + moves[i][0], c + moves[i][1]);
       }
     }
   return count;
+}
+
+public static void runTest(int i){
+
+  KnightBoard b;
+  int[]m =   {4,5,5,5,5};
+  int[]n =   {4,5,4,5,5};
+  int[]startx = {0,0,0,1,2};
+  int[]starty = {0,0,0,1,2};
+  int[]answers = {0,304,32,56,64};
+  if(i >= 0 ){
+    try{
+      int correct = answers[i];
+      b = new KnightBoard(m[i%m.length],n[i%m.length]);
+
+      int ans  = b.countSolutions(startx[i],starty[i]);
+
+      if(correct==ans){
+        System.out.println("PASS board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans);
+      }else{
+        System.out.println("FAIL board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans+" vs "+correct);
+      }
+    }catch(Exception e){
+      System.out.println("FAIL Exception case: "+i);
+
+    }
+  }
+}
+
+public static void main(String[] args) {
+  runTest(0);
+  runTest(1);
+  runTest(2);
+  runTest(3);
+  runTest(4);
+  runTest(5);
+}
 }
